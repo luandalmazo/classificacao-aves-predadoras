edaf02636b536e8e4147f167e20fa7d559e8aacc
luandalmazo <luandalmazo@gmail.com>
2024-11-24 15:31:08 -0300

    update tests

--- test_families.py
+++ test_families.py
@@ -1,4 +1,5 @@
 import torch
+import numpy as np
 from torchvision import datasets, transforms
 from torchvision import models
 from torch import nn
@@ -6,6 +7,8 @@ import os
 from dotenv import load_dotenv
 from sklearn.metrics import precision_score, f1_score, recall_score
 from torch.utils.data import DataLoader
+import matplotlib.pyplot as plt
+import seaborn as sns
 
 set_transforms = {  
     'test': transforms.Compose([
@@ -20,58 +23,94 @@ TEST_DIR = os.getenv('TEST_DIR')
 dataset_test = datasets.ImageFolder(TEST_DIR, transform=set_transforms['test'])
 dataloader_test = DataLoader(dataset_test, batch_size=32, shuffle=False)
 
-''' Check if GPU is available '''
 def get_device():
     if torch.cuda.is_available():
         return torch.device('cuda')
     else:
         return torch.device('cpu')
 
+def test_model(model, dataloader_test, device):
+    model.to(device)
+    model.eval()
+    true_labels = []
+    predicted_labels = []
+    with torch.no_grad():
+        for data in dataloader_test:
+            images, labels = data
+            images, labels = images.to(device), labels.to(device)
+            outputs = model(images)
+            _, predicted = torch.max(outputs.data, 1)
+            true_labels.extend(labels.cpu().numpy())
+            predicted_labels.extend(predicted.cpu().numpy())
 
-''' Testing the model '''
-print("Starting...")
-model = models.resnet50()
-model.fc = nn.Linear(2048, 6)
-model.load_state_dict(torch.load("./best_model.pth"))
-correct = 0
-total = 0
+    precision = precision_score(true_labels, predicted_labels, average='macro')
+    f1 = f1_score(true_labels, predicted_labels, average='macro')
+    recall = recall_score(true_labels, predicted_labels, average='macro')
+    precision_class = precision_score(true_labels, predicted_labels, average=None)
+    f1_class = f1_score(true_labels, predicted_labels, average=None)
+    recall_class = recall_score(true_labels, predicted_labels, average=None)
+
+    return precision, f1, recall, precision_class, f1_class, recall_class
+
 device = get_device()
-model.to(device)
-model.eval()
-true_labels = []
-predicted_labels = []
+model_paths = ["./best_model_1.pth", "./best_model_2.pth", "./best_model_3.pth", "./best_model_4.pth", "./best_model_5.pth"]
 
-with torch.no_grad():
-    for data in dataloader_test:
-        images, labels = data
-        images, labels = images.to(device), labels.to(device)
-        outputs = model(images)
-        _, predicted = torch.max(outputs.data, 1)
-        total += labels.size(0)
-        correct += (predicted == labels).sum().item()
+all_precision = []
+all_f1 = []
+all_recall = []
+all_precision_class = []
+all_f1_class = []
+all_recall_class = []
 
-        true_labels.extend(labels.cpu().numpy())
-        predicted_labels.extend(predicted.cpu().numpy())
+for model_path in model_paths:
+    model = models.resnet50()
+    model.fc = nn.Linear(2048, 41)
+    model.load_state_dict(torch.load(model_path))
+    
+    precision, f1, recall, precision_class, f1_class, recall_class = test_model(model, dataloader_test, device)
+    
+    all_precision.append(precision)
+    all_f1.append(f1)
+    all_recall.append(recall)
+    all_precision_class.append(precision_class)
+    all_f1_class.append(f1_class)
+    all_recall_class.append(recall_class)
 
+mean_precision = np.mean(all_precision)
+std_precision = np.std(all_precision)
 
-precision = precision_score(true_labels, predicted_labels, average='macro')
-f1 = f1_score(true_labels, predicted_labels, average='macro')
-recall = recall_score(true_labels, predicted_labels, average='macro')
-precision_class = precision_score(true_labels, predicted_labels, average=None)
-f1_class = f1_score(true_labels, predicted_labels, average=None)
-recall_class = recall_score(true_labels, predicted_labels, average=None)
+mean_f1 = np.mean(all_f1)
+std_f1 = np.std(all_f1)
 
-print(f"Precisão: {precision}")
-print(f"F1: {f1}")
-print(f"Recall: {recall}")
-print("Precisão por classe:")
+mean_recall = np.mean(all_recall)
+std_recall = np.std(all_recall)
+
+mean_precision_class = np.mean(all_precision_class, axis=0)
+std_precision_class = np.std(all_precision_class, axis=0)
+
+mean_f1_class = np.mean(all_f1_class, axis=0)
+std_f1_class = np.std(all_f1_class, axis=0)
+
+mean_recall_class = np.mean(all_recall_class, axis=0)
+std_recall_class = np.std(all_recall_class, axis=0)
+
+print("------------------------")
+print(f"Precisão média: {mean_precision:.2f} ± {std_precision:.2f}")
+print(f"F1 média: {mean_f1:.2f} ± {std_f1:.2f}")
+print(f"Recall médio: {mean_recall:.2f} ± {std_recall:.2f}")
+
+print("\nPrecisão média por classe:")
 for idx, class_name in enumerate(dataset_test.classes):
-    print(f"{class_name}: {precision_class[idx]}")
-print("F1 por classe:")
+    print(f"{class_name}: {mean_precision_class[idx]:.2f} ± {std_precision_class[idx]:.2f}")
+print("------------------------")
+
+print("F1 médio por classe:")
 for idx, class_name in enumerate(dataset_test.classes):
-    print(f"{class_name}: {f1_class[idx]}")
-print("Recall por classe:")
+    print(f"{class_name}: {mean_f1_class[idx]:.2f} ± {std_f1_class[idx]:.2f}")
+print("------------------------")
+
+print("Recall médio por classe:")
 for idx, class_name in enumerate(dataset_test.classes):
-    print(f"{class_name}: {recall_class[idx]}")
+    print(f"{class_name}: {mean_recall_class[idx]:.2f} ± {std_recall_class[idx]:.2f}")
 
 
